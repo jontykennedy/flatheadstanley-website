6b300210959bc38f5703b56b7722fe6498fbba53
jontykennedy <accounts@jontykennedy.co.uk>
2023-12-10 21:39:28 +0100

    Add newsletter subscription form

--- script.js
+++ script.js
@@ -1,13 +1,88 @@
- const updateLocalTime = () => {
-    const options = {
-      dateStyle: "medium",
-      timeStyle: "long",
-      timeZone: 'Europe/Copenhagen'
-    };
-      
-    const copenhagenTime = new Date().toLocaleString('en-GB', options);
-    document.getElementById('localTime').innerHTML = '<b>Copenhagen, Denmark:</b> ' + copenhagenTime;
-  };
+const subscribe = async (email, errorLabel, successLabel) => {
+  try {
+    const response = await fetch(`https://dn6jaadg7vtvplqlq32zlj7g4u0fvbhw.lambda-url.eu-west-2.on.aws/subscribe?email=${encodeURIComponent(email)}`);
+
+    if (!response.ok) {
+      if (response.status === 409) {
+        successLabel.innerHTML = "You're already subscribed, check your emails for a verification link";
+        return;
+      }
+
+      throw new Error();
+    }
+
+    successLabel.innerHTML = "Thanks, look out for a verification email";
+  } catch (error) {
+    errorLabel.innerHTML = "An error occurred, try again later";
+  }
+};
+
+const unsubscribe = async (email, errorLabel, successLabel) => {
+  try {
+    const response = await fetch(`https://dn6jaadg7vtvplqlq32zlj7g4u0fvbhw.lambda-url.eu-west-2.on.aws/unsubscribe?email=${encodeURIComponent(email)}`);
+
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    successLabel.innerHTML = "Unsubscribed successfully";
+  } catch (error) {
+    errorLabel.innerHTML = "An error occurred, try again later";
+  }
+};
+
+const submitNewsletterRequest = (e) => {
+  const errorLabel = document.getElementById('email-error-label');
+  const successLabel = document.getElementById('email-success-label');
+  errorLabel.innerHTML = "";
+  successLabel.innerHTML = "";
+
+  const email = new FormData(e.target).entries().next().value[1];
   
-  setInterval(updateLocalTime, 1000);
-  window.addEventListener('DOMContentLoaded', updateLocalTime);
+  if (email.length === 0 || !email.includes("@")) {
+    errorLabel.innerHTML = "Please enter a valid email!";
+  } else {
+    subscribe(email, errorLabel, successLabel);
+  }
+
+  const emailForm = document.getElementById('email');
+  emailForm.value = "";
+
+  return;
+};
+
+const submitUnsubscribeRequest = (e) => {
+  const errorLabel = document.getElementById('email-error-label');
+  const successLabel = document.getElementById('email-success-label');
+  errorLabel.innerHTML = "";
+  successLabel.innerHTML = "";
+
+  const email = new FormData(e.target).entries().next().value[1];
+  
+  if (email.length === 0 || !email.includes("@")) {
+    errorLabel.innerHTML = "Please enter a valid email!";
+  } else {
+    unsubscribe(email, errorLabel, successLabel);
+  }
+
+  const emailForm = document.getElementById('email');
+  emailForm.value = "";
+
+  return;
+};
+
+const updateLocalTime = () => {
+  const options = {
+    dateStyle: "medium",
+    timeStyle: "long",
+    timeZone: 'Europe/Copenhagen'
+  };
+    
+  const copenhagenTime = new Date().toLocaleString('en-GB', options);
+  document.getElementById('localTime').innerHTML = '<b>Copenhagen, Denmark:</b> ' + copenhagenTime;
+};
+  
+setInterval(updateLocalTime, 1000);
+window.addEventListener('DOMContentLoaded', updateLocalTime);
+
+

e37549717b2dd0f21481e11072fadb2a932e2e80
jontykennedy <accounts@jontykennedy.co.uk>
2023-12-10 14:25:28 +0100

    Add 404 page and music downloads (without links)

---
+++ script.js
@@ -0,0 +1,13 @@
+ const updateLocalTime = () => {
+    const options = {
+      dateStyle: "medium",
+      timeStyle: "long",
+      timeZone: 'Europe/Copenhagen'
+    };
+      
+    const copenhagenTime = new Date().toLocaleString('en-GB', options);
+    document.getElementById('localTime').innerHTML = '<b>Copenhagen, Denmark:</b> ' + copenhagenTime;
+  };
+  
+  setInterval(updateLocalTime, 1000);
+  window.addEventListener('DOMContentLoaded', updateLocalTime);

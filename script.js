9bdec972d1a4d6627c222e12836c445358c391bf
Jonty Kennedy <accounts@jontykennedy.co.uk>
2024-12-20 12:45:10 +0100

    Change links page layout and edits to content

--- script.js
+++ script.js
@@ -35,17 +35,3 @@ const submitNewsletterRequest = (e) => {
   emailForm.value = "";
 
 };
-
-const updateLocalTime = () => {
-  const options = {
-    dateStyle: "medium",
-    timeStyle: "long",
-    timeZone: 'Europe/Copenhagen'
-  };
-    
-  const copenhagenTime = new Date().toLocaleString('en-GB', options);
-  document.getElementById('localTime').innerHTML = '<b>Copenhagen, Denmark:</b> ' + copenhagenTime;
-};
-  
-setInterval(updateLocalTime, 1000);
-window.addEventListener('DOMContentLoaded', updateLocalTime);
